d34317aaf07743319d84153d50c10d49f4f1665a
Leonardo Saippa <leonardomsaippa@gmail.com>
2021-04-19 19:49:43 -0300

    Initial Commit

---
+++ BestCars/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  BestCars
+//
+//  Created by Leonardo Saippa on 19/04/21.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+        
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
