9c0062baa92c9d275e06d9f7f68e95e5089784eb
Leonardo Saippa <leonardomsaippa@gmail.com>
2021-04-27 19:46:20 -0300

    Update AppDelegate

--- BestCars/AppDelegate.swift
+++ BestCars/AppDelegate.swift
@@ -6,16 +6,32 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-        
+    let dataController = DataController(modelName: "BestCars")
+    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        DataControllerInstance.dataControllerInstance.setDataController(currentData: (UIApplication.shared.delegate as? AppDelegate)?.dataController)
+        dataController.load()
         return true
     }
 
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        saveViewContext()
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        saveViewContext()
+    }
+
+    func saveViewContext() {
+        try? dataController.viewContext.save()
+    }
 
 }
 
